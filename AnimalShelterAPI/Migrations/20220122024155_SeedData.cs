5e299139153b027fc8282a72173059fb3e4ea780
Albert Lee <leesga8@gmail.com>
2022-01-21 18:43:04 -0800

    seed data

---
+++ AnimalShelterAPI/Migrations/20220122024155_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 15, "Male", "Bob", "Dog" },
+                    { 2, 10, "Male", "Peter", "Cat" },
+                    { 3, 2, "Female", "Jane", "Cat" },
+                    { 4, 7, "Female", "Mary", "Dog" },
+                    { 5, 12, "Male", "John", "Duck" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
--- AnimalShelterAPI/Models/AnimalShelterAPIContext.cs
+++ AnimalShelterAPI/Models/AnimalShelterAPIContext.cs
@@ -2,13 +2,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelterAPI.Models
 {
-    public class AnimalShelterAPIContext : DbContext
+  public class AnimalShelterAPIContext : DbContext
+  {
+    public AnimalShelterAPIContext(DbContextOptions<AnimalShelterAPIContext> options)
+        : base(options)
     {
-        public AnimalShelterAPIContext(DbContextOptions<AnimalShelterAPIContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Animal> Animals { get; set; }
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+          .HasData(
+              new Animal { AnimalId = 1, Name = "Bob", Species = "Dog", Age = 15, Gender = "Male" },
+              new Animal { AnimalId = 2, Name = "Peter", Species = "Cat", Age = 10, Gender = "Male" },
+              new Animal { AnimalId = 3, Name = "Jane", Species = "Cat", Age = 2, Gender = "Female" },
+              new Animal { AnimalId = 4, Name = "Mary", Species = "Dog", Age = 7, Gender = "Female" },
+              new Animal { AnimalId = 5, Name = "John", Species = "Duck", Age = 12, Gender = "Male" }
+          );
+    }
+    public DbSet<Animal> Animals { get; set; }
+  }
 }
